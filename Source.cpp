c40bcadf1f366cf3d2daf8e3ab00258ae46f3a97
Katrinnk <katerin69llobster@gmail.com>
2020-11-08 14:45:51 +0200

    5/1

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+// Lab 5_1
+//Колмикова Катерина
+// варіант 12
+#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double a, const double b, const double c); // прототип
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double z = (f(1, t + s, s) + f(t, s * t, 1)) / (1 + pow(f(s, 1, t), 2));
+	cout << "z = " << z << endl;
+	return 0;
+}
+double f(const double a, const double b, const double c) // визначення
+{
+	return (a * sin(b) + b * sin(a) + c * c);
+}
